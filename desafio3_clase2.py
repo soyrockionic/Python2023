49ee18a14f4cea1c47169af6fbc341f53b5aa683
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2023-03-21 22:57:02 -0300

    Desafio 3 clase 2

---
+++ desafio3_clase2.py
@@ -0,0 +1,23 @@
+print("*"*30)
+pelis_harry = []
+tmp = int(input("Ingrese duracion "))
+while tmp != 0 :
+    nombre = input("Ingrese nombre de la pelicula ")
+    pelis_harry.append([nombre,tmp])
+    tmp = int(input("Ingrese duracion "))
+print("*"*30)
+print(pelis_harry)
+
+print("*"*30)
+suma = 0
+for i in pelis_harry :
+    suma = suma + i[1]
+print(f"Promedio {suma / len(pelis_harry)}")
+
+print("*"*30)
+promedio = suma / len(pelis_harry)
+cont = 0
+for i in pelis_harry :
+    if(i[1] > promedio):
+        cont = cont + 1
+print(f"Cantidad de pelis que duran mas que el promedio {cont}")
