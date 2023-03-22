3081a4104715bbfa277ef140557a5cdcdae0c670
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2023-03-22 13:14:32 -0300

    Desafio 5 clase 2 - con funciones

--- desafio5_con_funciones_clase2.py
+++ desafio5_con_funciones_clase2.py
@@ -8,7 +8,7 @@ def ingreso_pelis():
     return pelis
 
 def promedio(pelis_harry):
-    return sum(pelis_harry[i] for i in pelis_harry)
+    return sum(pelis_harry[i] / len(pelis_harry) for i in pelis_harry)
 
 def mayor_promedio(pelis_harry):
     return [(i,pelis_harry[i]) for i in pelis_harry if pelis_harry [i] > promedio(pelis_harry)]
