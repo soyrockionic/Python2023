296a34576199243acd0907443bf48de2c805a5fc
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2023-04-04 01:20:29 -0300

    Practica 2

---
+++ Practica_2/Ej6-P2.py
@@ -0,0 +1,8 @@
+linea = "-"*30
+print(linea)
+palabra = input("Ingrese una palabara ")
+if "a" in palabra and "n" in palabra:
+    print("Contiene las letras a y n")
+else:
+    print("No contiene las 2 letras")
+print(linea)
