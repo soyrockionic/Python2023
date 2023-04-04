5427145a623a23a6188c5e4e36c2e78339e52947
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2023-04-04 01:21:31 -0300

    Delete Practica_2 directory

--- Practica_2/Ej5-P2.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-
-frase = input("ingrese una frase: ").lower()
-pal = input("ingrese una palabra: ").lower()
-
-print(f"{pal} esta {Counter(frase.split())[pal]} veces en la frase")
--- Practica_2/Ej6-P2.py
+++
@@ -1,8 +0,0 @@
-linea = "-"*30
-print(linea)
-palabra = input("Ingrese una palabara ")
-if "a" in palabra and "n" in palabra:
-    print("Contiene las letras a y n")
-else:
-    print("No contiene las 2 letras")
-print(linea)
