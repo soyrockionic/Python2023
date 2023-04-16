cdc7ca67fe47da2e2f74571617d817e6c871d471
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2023-04-16 13:21:08 -0300

    Clase 5 - Ejemplo con GUI

--- clase5_gui.py
+++ clase5_gui.py
@@ -9,16 +9,17 @@ enc = next(arch)
 def producciones(pelis):
     for peli in arch:
         if "2019" == peli[7] and "Argentina" == peli[5]:
-            pelis = pelis + f"{peli[2]:<45} {peli[1]}" + "\n"
+            pelis = pelis + f"{peli[2]:<45} {peli[1]:<4}" + "\n"
     return pelis
 
 titles = "Producciones argentinas 2019 - Netflix"
 pelis = ""
 layout = [[sg.Text(titles + "\n" + "-"*72)], [sg.Text(producciones(pelis))], [sg.Button("Salir")]]
 
+archivo.close()
+
 window = sg.Window("Informe - Netflix", layout, size=(600,450), margins=(130,60))
 
 event, values = window.read()
 
-arch.close()
 window.close()
