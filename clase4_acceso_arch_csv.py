97e3af60e913866b6260104a2f9fb7b4d5938060
soyrockionic <44347792+soyrockionic@users.noreply.github.com>
2023-04-27 02:03:06 -0300

    Clase 4 - Archivos csv

--- clase4_acceso_arch_csv.py
+++ clase4_acceso_arch_csv.py
@@ -1,20 +1,17 @@
 import csv
 
-archivo = open('netflix_titles.csv', "r",encoding='utf-8')
-csvreader = csv.reader(archivo, delimiter=',')
-
-encabezado = next(csvreader)
+with open('netflix_titles.csv', "r",encoding='utf-8') as archivo:
+    arch = csv.reader(archivo, delimiter=',')
+    encabezado, datos = next(arch), list(arch)
 
 print()
 print(f"{encabezado[2]:<45}  {encabezado[3]}")
 print(f"{'-'*45}  {'-'*33}")
 
-#for linea in csvreader:
+#for linea in datos:
     #if linea[1] == "Movie" and linea[5] == "Argentina":
         #print(f"{linea[2]:<45}  {linea[3]}")
 
-shows_ar = filter(lambda x: x[5] == "Argentina" and x[1] == "Movie", csvreader)
+shows_ar = filter(lambda x: x[5] == "Argentina" and x[1] == "Movie", datos)
 for elem in shows_ar:
     print(f"{elem[2]:<45}  {elem[3]}")
-
-archivo.close()
